2998eb59786b1e3600d2b3d0bdb224c823789c2a
Nikel Arteta <nikelarteta@Senspark-Mini-04s-Mac-mini.local>
2016-06-16 20:03:27 +0700

    - Init store-listing-toolkit.

---
+++ gplay/run_populate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os, os.path
+
+cmd = "../../app-stores-toolkit/populate.py metadata -platform android -prj-path gplay -data-file-path src/goldminer-classic-origin-metadata-translate.xlsx -customized-metadata-path src/gplay/metadata"
+print cmd
+os.system(cmd)
